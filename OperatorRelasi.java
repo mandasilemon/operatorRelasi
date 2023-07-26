60fbebe1cceff07f986ee7d9e25224f0d2a99a58
mandasilemon <110959957+mandasilemon@users.noreply.github.com>
2023-07-27 05:45:20 +0700

    Add files via upload

---
+++ OperatorRelasi.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.operatorrelasi;
+import java.util.Scanner;
+public class OperatorRelasi {
+
+    public static void main(String[] args) {
+        int nilai1 = 50;
+        int nilai2 = 20;
+        System.out.println("Nilai1 > nilai2 = "+(nilai1>nilai2));
+        System.out.println("Nilai1 >= nilai2 =  "+(nilai1>=nilai2));
+        System.out.println("Nilai1 == nilai2 = "+(nilai1== nilai2));
+        System.out.println("nilai1 < nilai2 = "+(nilai1<nilai2));
+        System.out.println("nilai1 <= nilai2 = "+(nilai1<=nilai2));
+        System.out.println("nilai1 != nilai2 = "+(nilai1!=nilai2));
+          
+        
+    }
+}
